f3a2c6a666c0ec4e867f04ab83859f6df38b9224
Dariya <165070852+Dashayan003@users.noreply.github.com>
2024-12-24 23:28:19 +0700

    Add files via upload

---
+++ module_13_5.py
@@ -0,0 +1,69 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+import asyncio
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+button_calculate = types.KeyboardButton(text="Рассчитать")
+button_info = types.KeyboardButton(text="Информация")
+keyboard.add(button_calculate, button_info)
+
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    await message.answer(
+        "Привет! Я бот, помогающий твоему здоровью. Выберите действие:",
+        reply_markup=keyboard
+    )
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text=['Рассчитать'])
+async def set_age(message: types.Message):
+    await message.answer("Введите свой возраст:")
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer("Введите свой рост:")
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer("Введите свой вес:")
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+
+    age = int(data['age'])
+    growth = int(data['growth'])
+    weight = int(data['weight'])
+
+    calories = 10 * weight + 6.25 * growth - 5 * age - 161
+
+    await message.answer(f"Ваша норма калорий: {calories} ккал.")
+
+    await state.finish()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
